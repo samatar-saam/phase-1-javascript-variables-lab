8c20dc342debae5b85ecfb92ae167d80032b54bd
samatar mohamed <samatarmohamed59@gmail.com>
2025-12-23 16:11:23 +0300

    Complete Variables Lab - all tests passing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
-const companyName = 'Scuber';
-console.log(companyName)
-let companyCeo='Susan Smith'
-console.log(companyCeo)
-let mostProfitableNeighborhood='Chelsea'
-console.log(mostProfitableNeighborhood)
+const companyName = "Scuber";
+console.log(companyName);
+let companyCeo = "Susan Smith";
+console.log(companyCeo);
+let mostProfitableNeighborhood = "Chelsea";
+console.log(mostProfitableNeighborhood);
